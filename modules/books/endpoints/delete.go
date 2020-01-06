30a71d51c7ed311100edc4d0490d176d14dda799
Matheus Fidelis <matheus@nanoshots.com.br>
2020-01-06 18:18:15 -0300

    [DELETE]

--- modules/books/endpoints/delete.go
+++ modules/books/endpoints/delete.go
@@ -9,13 +9,8 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
