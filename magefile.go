cc3c633321e1271d563d3a57954344c4613fc0be
Josh Quintana <joshq00@gmail.com>
2021-10-22 02:22:33 -0400

    Make it not dependent on make

---
+++ magefile.go
@@ -0,0 +1,64 @@
+// +build mage
+
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+var Default = PHONY
+
+func PHONY() {
+	mg.Deps(Clean, Build, Deploy)
+}
+
+func def() {
+	mg.Deps(Clean, Build, Deploy)
+}
+
+// clean remove all bin
+func Clean() error {
+	return sh.Rm("bin")
+}
+
+// build build each module into "bin"
+func Build() error {
+	mg.Deps(Clean)
+	os.Setenv("GO111MODULE", "on")
+	os.Setenv("GOOS", "linux")
+	gobuild := sh.RunCmd("go", "build", "-ldflags", "-s -w", "-o")
+
+	bins := []struct {
+		output string
+		source string
+	}{
+		{"bin/books/endpoints/create", "modules/books/endpoints/create.go"},
+		{"bin/books/endpoints/read", "modules/books/endpoints/read.go"},
+		{"bin/books/endpoints/detail", "modules/books/endpoints/detail.go"},
+		{"bin/books/endpoints/update", "modules/books/endpoints/update.go"},
+		{"bin/books/endpoints/delete", "modules/books/endpoints/delete.go"},
+
+		{"bin/books/functions/worker", "modules/books/functions/worker.go"},
+	}
+	for _, bin := range bins {
+		err := gobuild(bin.output, bin.source)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// test run all go tests
+func Test() error {
+	return sh.Run("go", "test", "./...")
+}
+
+// deploy force deployment to default
+func Deploy() error {
+	mg.Deps(Clean, Build)
+	return sh.Run("serverless", "deploy", "--verbose", "--force")
+}
